15ad23613517d04278762707bb0ff6fea571d4be
Efen Stakes <efenstakes101@gmail.com>
2020-05-01 18:32:12 +0300

    add food model

---
+++ lib/models/food.dart
@@ -0,0 +1,17 @@
+
+
+class Food {
+  String id;
+  String name;
+  String price;
+  String description;
+  String type;
+
+  Food({
+    this.id, 
+    this.name,
+    this.price,
+    this.description,
+    this.type,
+  });
+}
